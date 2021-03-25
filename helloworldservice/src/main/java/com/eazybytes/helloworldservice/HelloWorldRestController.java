5379d03cef844f763e2ea3341f59262ca044a274
Eazy Bytes <tutor@eazybytes.com>
2021-03-25 15:46:13 +0530

    Intial Commit

--- helloworldservice/src/main/java/com/eazybytes/helloworldservice/HelloWorldRestController.java
+++ helloworldservice/src/main/java/com/eazybytes/helloworldservice/HelloWorldRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldRestController {
 
 	@GetMapping(value = "/hello")
-	public String sayHelloMsg() {
+	public String sayHello() {
 		return "Hello, Welcome to course on Microservices";
 	}
 }

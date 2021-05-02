36a029062910b093484002d3f0620f7063499bf8
Eazy Bytes <tutor@eazybytes.com>
2021-05-02 20:53:59 +0530

    Thanks for choosing to learn from EazyBytes

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,8 @@
  */
 package com.eazybytes.accounts.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.eazybytes.accounts.config.AccountsServiceConfig;
 import com.eazybytes.accounts.model.Accounts;
+import com.eazybytes.accounts.model.Cards;
 import com.eazybytes.accounts.model.Customer;
+import com.eazybytes.accounts.model.CustomerDetails;
+import com.eazybytes.accounts.model.Loans;
 import com.eazybytes.accounts.model.Properties;
 import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.service.client.CardsFeignClient;
+import com.eazybytes.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -31,6 +38,12 @@ public class AccountsController {
 
 	@Autowired
 	AccountsServiceConfig accountsConfig;
+
+	@Autowired
+	LoansFeignClient loansFeignClient;
+
+	@Autowired
+	CardsFeignClient cardsFeignClient;
 
 	@PostMapping("/myAccount")
 	public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -51,6 +64,22 @@ public class AccountsController {
 				accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
 		String jsonStr = ow.writeValueAsString(properties);
 		return jsonStr;
+	}
+
+	@PostMapping("/myCustomerDetails")
+	public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+
+		Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+		List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+		List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+		CustomerDetails customerDetails = new CustomerDetails();
+		customerDetails.setAccounts(accounts);
+		customerDetails.setLoans(loans);
+		customerDetails.setCards(cards);
+
+		return customerDetails;
+
 	}
 
 }
